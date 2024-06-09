42bc8d43effd92c67f1c180cd470bb087d51ce60
Angel Qiu <angelqiu1207@gmail.com>
2024-06-08 21:02:56 -0400

    synced timer and pig

--- main.py
+++ main.py
@@ -17,6 +17,17 @@ def main():
     sidestretch2 = PhotoImage(file="sidestretch2.png")
     squat1 = PhotoImage(file="squat1.png")
     squat2 = PhotoImage(file="squat2.png")
+    lobby = PhotoImage(file="mainpage.png")
+
+    def drawIntro():
+        s.create_image(400, 300, image=lobby)
+    
+    # root.bind("<Button-1>", startScreenClick)
+    # def startScreenClick(event):
+    drawIntro()
+
+
+            
 
     relations = {
         "jumpingjacks1": jumpingjacks1,
@@ -54,29 +65,33 @@ def main():
         root.after(1000, lambda: deleteImage(jump2))
         root.after(1000, lambda: moveOne(repeat-1))
 
-    moveOne(10)
             
     #Function to update the timer
     countdown_text = s.create_text(350, 55, text="", fill="black", font="Arial 16")
 
     def timer(secs, mins):
-            global countdown_text
-            if secs >= 0 or mins >= 0:
-                format = '{:02d}:{:02d}'.format(mins, secs)
-                s.itemconfig(countdown_text, text=format)
-                if secs == 0 and mins > 0:
+        global countdown_text
+        if mins > 0 or secs > 0:
+            format = '{:02d}:{:02d}'.format(mins, secs)
+            s.itemconfig(countdown_text, text=format)
+            if secs == 0:
+                if mins > 0:
                     mins -= 1
                     secs = 59
-                else:
-                    secs -= 1
-                # Next update 1 sec later
-                if secs >= 0 or mins >= 0:
-                    root.after(1000, lambda: timer(secs, mins))
             else:
-                # Timer end
-                s.itemconfig(countdown_text, text="00:00")
+                secs -= 1
+            # Schedule the next update 1 sec later
+            root.after(1000, lambda: timer(secs, mins))
+        else:
+            # Timer end
+            s.itemconfig(countdown_text, text="00:00")
 
-    timer(20, 0)
+    # Start the timer with 20 seconds
+    workoutTime = randint(15,30)*2
+    timer(workoutTime, 0)
+    moveOne(workoutTime/2)
+    
+
 
 
 
